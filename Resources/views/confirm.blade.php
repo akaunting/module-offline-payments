ec45f85baf6077f7d2c0719cee4911bdfb0ed2f2
Cüneyt Şentürk <cneytsenturk@gmail.com>
2021-11-08 11:10:18 +0300

    close #8 Fixed: Offline Payment Description not being displayed.

--- Resources/views/confirm.blade.php
+++ Resources/views/confirm.blade.php
@@ -1,7 +1,7 @@
 <h2>{{ $setting['name'] }}</h2>
 
 @if ($setting['description'])
-    <div class="well well-sm">
+    <div class="well well-sm mt-2 blockquote">
         {{ $setting['description'] }}
     </div>
 @endif
--- Resources/views/show.blade.php
+++ Resources/views/show.blade.php
@@ -5,7 +5,7 @@
         @endif
 
         @if (!empty($setting['description']))
-            <div class="well well-sm">{{ $setting['description'] }}</div>
+            <div class="well well-sm mt-2 blockquote">{{ $setting['description'] }}</div>
         @endif
     </div>
     <br>
